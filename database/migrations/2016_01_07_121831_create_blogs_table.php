c9554a12efa272c40af368835ba6a9e823b9a0b8
Subhankar <subhankar.bachchu@gmail.com>
2016-01-07 18:55:39 +0530

    07-01-2015

---
+++ database/migrations/2016_01_07_121831_create_blogs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->longText('content');   
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('blogs');
+    }
+}
